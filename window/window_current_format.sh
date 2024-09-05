b76afea7393e3d9aba260892dc5c16e4700f6227
Paliak <91493239+Paliak@users.noreply.github.com>
2024-09-05 12:26:01 +0200

    CUSTOM: change window status to window name

--- window/window_current_format.sh
+++ window/window_current_format.sh
@@ -15,7 +15,7 @@ show_window_current_format() {
   number="#I"
   color=$(get_tmux_batch_option "@catppuccin_window_current_color" "$thm_orange")
   background=$(get_tmux_batch_option "@catppuccin_window_current_background" "$thm_bg")
-  text="$(get_tmux_batch_option "@catppuccin_window_current_text" "#{b:pane_current_path}")" # use #W for application instead of directory
+  text="$(get_tmux_batch_option "@catppuccin_window_current_text" "#W")" # use #W for application instead of directory
   fill="$(get_tmux_batch_option "@catppuccin_window_current_fill" "number")"                 # number, all, none
 
   current_window_format=$(build_window_format "$number" "$color" "$background" "$text" "$fill")
--- window/window_default_format.sh
+++ window/window_default_format.sh
@@ -15,7 +15,7 @@ show_window_default_format() {
   number="#I"
   color=$(get_tmux_batch_option "@catppuccin_window_default_color" "$thm_blue")
   background=$(get_tmux_batch_option "@catppuccin_window_default_background" "$thm_gray")
-  text="$(get_tmux_batch_option "@catppuccin_window_default_text" "#{b:pane_current_path}")" # use #W for application instead of directory
+  text="$(get_tmux_batch_option "@catppuccin_window_default_text" "#W")" # use #W for application instead of directory
   fill="$(get_tmux_batch_option "@catppuccin_window_default_fill" "number")"                 # number, all, none
 
   default_window_format=$(build_window_format "$number" "$color" "$background" "$text" "$fill")
